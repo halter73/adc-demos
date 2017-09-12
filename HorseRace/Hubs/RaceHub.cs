00d8b78607c30caa8d8da2b1833e3465aa0e8690
Stephen Halter <halter73@gmail.com>
2017-09-12 12:00:59 -0700

    Add server hub invocation.

--- HorseRace/Hubs/RaceHub.cs
+++ HorseRace/Hubs/RaceHub.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 
 namespace HorseRace.Hubs
 {
     public class RaceHub : Hub
     {
+        public async Task<string> SendMessage(string message)
+        {
+            await Clients.All.InvokeAsync("OnMessage", $"{Context.ConnectionId} {message}");
+            return Context.ConnectionId;
+        }
     }
 }
