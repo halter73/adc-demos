50556879a5af5a30a1f6dbdbc0d3e11659edc0e7
Stephen Halter <halter73@gmail.com>
2017-09-12 02:03:15 -0700

    Raw presence

---
+++ SignalR/HorseRace/Controllers/PresenceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HorseRace.Controllers
+{
+    [Route("api/[controller]")]
+    public class PresenceController
+    {
+        private readonly RawEndPoint _rawEndPoint;
+
+        public PresenceController(RawEndPoint rawEndPoint)
+        {
+            _rawEndPoint = rawEndPoint;
+        }
+
+        [HttpGet]
+        public IEnumerable<(string, string)> GetRawConnections()
+        {
+            return _rawEndPoint.Connections.Select(context =>
+                (context.ConnectionId,
+                context.Metadata.TryGetValue("LastSent", out var lastSent) ? (string)lastSent : "<empty>"));
+        }
+    }
+}
