d55519f911ffcf190c037f6ccc2dcd2ea886e44e
Stephen Halter <shalter@Stephens-MacBook-Pro.local>
2017-09-18 06:03:24 +0200

    Update MemoryLeakController

--- Linux/BasicWeb/Controllers/MemoryLeakController.cs
+++ Linux/BasicWeb/Controllers/MemoryLeakController.cs
@@ -10,22 +10,17 @@ namespace BasicWeb.Controllers
     [Route("api/[controller]")]
     public class MemoryLeakController : Controller
     {
-        private static readonly ConcurrentDictionary<string, int> _dict = new ConcurrentDictionary<string, int>();
+        private static readonly Holder _holder = new Holder();
 
         [HttpGet]
         public int Get()
         {
-            HttpContext.Response.Headers["Connection"] = "close";
-            return _dict.AddOrUpdate(HttpContext.Connection.Id, 0, (id, count) => count + 1);
+            return _holder.Dictionary.AddOrUpdate(HttpContext.TraceIdentifier, 0, (id, count) => count + 1);
         }
 
-        // private static readonly ConcurrentDictionary<string, (HttpContext, int)> _dict = new ConcurrentDictionary<string, (HttpContext, int)>();
-
-        // [HttpGet]
-        // public int Get()
-        // {
-        //     HttpContext.Response.Headers["Connection"] = "close";
-        //     return _dict.AddOrUpdate(HttpContext.Connection.Id, (HttpContext, 0), (id, tuple) => (tuple.Item1, tuple.Item2 + 1)).Item2;
-        // }
+        private class Holder
+        {
+            public readonly ConcurrentDictionary<string, int> Dictionary = new ConcurrentDictionary<string, int>();
+        }
     }
 }
